636f06fdd64ef55e549255ffd782ec56990ddf09
Harman604-pixel <kharmanjeet832@gmail.com>
2025-04-22 13:16:17 -0400

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import styles from '../styles/Navbar.module.css';
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>CPAN144 Project</div>
+      <ul className={styles.navLinks}>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
